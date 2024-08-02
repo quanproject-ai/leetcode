3068a765d35522ba5d96754ebf997a299e06ca7e
Quan Ha <quanhan.anh@gmail.com>
2024-08-02 13:30:20 -0700

    fix user_retention.sq;

--- medium/sql/sqlinterviewquestion/user_retention.sql
+++ medium/sql/sqlinterviewquestion/user_retention.sql
@@ -52,7 +52,7 @@ INSERT INTO user_logins (user_id, login_date) VALUES
 (5, '2024-01-19');
 -- fetching data
 
-WITH chicken as (select 
+/*WITH chicken as (select 
 login_date, 
 count(user_id)  as total_login 
 from  user_logins
@@ -62,5 +62,37 @@ order by login_date
 
 SELECT 
   login_date,
-  total_login / (lead(total_login,1,total_login) over() )as user_retention
-FROM chicken
+  total_login,
+  (lead(total_login,1,total_login) over() / total_login)as user_retention
+FROM chicken*/
+with dup as (select 
+user_id,
+login_date, 
+row_number() over(partition by user_id order by login_date) as rn
+from  user_logins),
+
+count_date as (
+select 
+login_date, 
+count(user_id)  as total_login 
+from  user_logins
+group by login_date  
+order by login_date
+),
+verify_dup as (
+select 
+d2.user_id, d2.login_date
+from dup d1
+left join dup d2 on d1.user_id = d2.user_id and
+datediff(d2.login_date,d1.login_date) = 1 and
+d2.rn - d1.rn = 1
+where d2.user_id is not null
+)
+
+select
+(count(vd.user_id)/cd.total_login) as retention ,
+vd.login_date
+from verify_dup vd 
+left join count_date cd on cd.login_date = vd.login_date
+group by vd.login_date, cd.total_login
+
